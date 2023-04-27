33cac922f7d56b1e3b9594590fb4a41e10a4ccf9
LucasacuL <lcsriviere@gmail.com>
2023-04-27 14:04:05 +0200

    Enabled email

---
+++ js/submitForm.js
@@ -0,0 +1,21 @@
+function sendMail() {
+    var params = {
+      name: document.getElementById("name_contact").value,
+      email: document.getElementById("email_contact").value,
+      message: document.getElementById("message").value,
+    };  
+    const serviceID = "service_er9kof8";
+    const templateID = "template_zd583tu";
+  
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name_contact").value = "Your Name *";
+        document.getElementById("email_contact").value = "Your E-mail *";
+        document.getElementById("message").value = "Your Message *";
+        console.log(res);
+        alert("Your message has been sent successfully. \nWe will contact you soon. \n\nThank you!");
+        alert("name: {params.name} \nWe will contact you soon. \n\nThank you!");
+    })
+    .catch(err=>console.log(err));  
+  }
+  
